2411952900925947c8aa7d6d48d4830bcd6b11c6
Lukasz Piekarzewski <lukasz.piekarzewski@gmail.com>
2016-06-02 20:06:28 +0200

    Initial Commit

---
+++ com/graphic/framework/GraphicalFramework.java
@@ -0,0 +1,17 @@
+package com.graphic.framework;
+
+import java.awt.EventQueue;
+
+public class GraphicalFramework {
+	public static void main(String args[]) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				/*
+				 * Launching the CII Graphical Framework
+				 */
+				
+			}			
+		});
+	}
+}
+ 
